2f81d90b1258046406218a2323c82617f7b43b01
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-01-27 09:23:50 +0530

    Update package.json and package-lock.json with new dependencies; create user model file and adjust console log message in index.js

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ res.send('Hello, World!');
 });
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');  
+    console.log('Server is running on port 3000');  
 });
