6399808d16c0c6d1905492ecded06287beaeadbc
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-01-27 11:38:30 +0530

    Enhance index.js by adding middleware for static files and cookie parsing

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import { fileURLToPath } from "url";
+import path from "path";
+import cookieParser from 'cookie-parser';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
