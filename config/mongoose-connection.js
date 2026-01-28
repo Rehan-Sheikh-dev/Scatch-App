a8a4d92344624263c7d562b1c60d2c0ba0f13a24
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-01-28 10:07:18 +0530

    Add debug package and implement debugging for MongoDB connection

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,8 +1,9 @@
     import mongoose from 'mongoose';
-
+   
+    const debug = require("debug")("development:mongoose")
    const connectDB = async () => {
      try {
-   await mongoose.connect(`mongodb://${process.env.MONGODB_URI}/${process.env.DB_NAME}`,)
+   await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`,)
    console.log("MongoDB connected successfully");
     } catch (err) {
         console.log("Error connecting to MongoDB:", err);
