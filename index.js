2f9b5586a77cf8b18a14078bd1d505f35f891fd4
Rehan Sheikh <rehansheikh0808@gmail.com>
2026-01-29 15:30:14 +0530

    refactor: streamline express session middleware configuration

--- index.js
+++ index.js
@@ -20,21 +20,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
-app.use(
-    expressSession({
-     resave: false,
-     saveUninitialized: false,
-     secret: process.env.SESSION_SECRET,
-    }
-))
+app.use(expressSession({resave: false,saveUninitialized: false,secret: process.env.SESSION_SECRET,}))
 app.use('/owner',ownerRouter)
 app.use('/product',productRouter)
 app.use('/user',userRoter)
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-res.render('index');
-// res.send('Hello World!');
+// res.render('index');
 console.log(process.env.NODE_ENV);
 });
 
